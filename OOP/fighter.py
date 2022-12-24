d03d6ab8d8a224e7efa30e2425e3eddcd232f457
dartagnan sams <dartagnansams1@icloud.com>
2022-12-24 18:47:33 -0500

    created dict for sprite animations and placed images on screen

--- OOP/fighter.py
+++ OOP/fighter.py
@@ -2,12 +2,15 @@ import pygame
 from constants import *
 
 class Fighter:
-    def __init__(self,win,x,y,sheet,animation_steps):
+    def __init__(self,win,x,y,data,sheet,animation_steps):
         self.win = win
         self.x = x
         self.y = y
-        self.sheet = sheet
+        self.sprite_sheet = sheet
         self.animation_steps = animation_steps
+        self.sprite_size = data[0] #pixel size of sprite
+        self.image_scale = data[1]
+        self.image_offset = data[2]
         self.width = 50
         self.height = 150
         self.max_health = 100 #sets a placeholder for max health of player
@@ -19,6 +22,19 @@ class Fighter:
         self.attack_type = 0 
         self.enemy = None #gets the enemy object to be used to check collision 
         self.flip = False #check the position of the facing player returns a 0 or 1
+        self.animation_dict = self.load_images() #dict containing all spirte animations list 
+        self.action = 0 #0:idle,1:run,2:jump,3:attack1,4:attack2,5:hit,6:death
+        animation_names = {
+            "0":"idle",
+            "1":"run",
+            "2":"jump",
+            "3":"attack1",
+            "4":"attack2",
+            "5":"hit",
+            "6":"death"
+        } #defines animation name for individual animations
+        self.frame_index = 0 #what frame to start the image on
+        self.image = self.animation_dict[animation_names[str(self.action)]][self.frame_index] #returns the image from the animation list
 
     def draw_healthbar(self,x,y,health):
         pygame.draw.rect(self.win,(255,0,0),(x,y,health*2,30))
@@ -68,14 +84,32 @@ class Fighter:
             print("enemy hit")
             self.enemy.health -= 10
             print(self.enemy.health)
-            self.attacking = False
+            self.attacking = False #needs to be placed where after attacking can toggle back off
 
     def draw(self):
         self.rect = pygame.draw.rect(self.win,(0,255,0),pygame.Rect(self.x,self.y,self.width,self.height))  
+        self.win.blit(self.image,(self.x - (self.image_offset[0] * self.image_scale),self.y - (self.image_offset[1] * self.image_scale))) #draws the image on top of the rect
 
-    def load_images(self): #loads individual animations
-        for animation in self.animation_steps: #loops through all 
+
+    def load_images(self): #loads individual animations #0:idle,1:run,2:jump,3:attack1,4:attack2,5:hit,6:death
+        animation_names = {
+            "0":"idle",
+            "1":"run",
+            "2":"jump",
+            "3":"attack1",
+            "4":"attack2",
+            "5":"hit",
+            "6":"death"
+        } #defines animation name for individual animations
+        # animation_lst = []
+        animation_dict = {}
+        for y,animation in enumerate(self.animation_steps): #loops through all 
             temp_img_lst = []
             for x in range(animation): #loops throught the images left to right
-                temp_img = self.sprite_sheet.subsurface(x * 162,0,162,162) #subsurface grabs a area around a image given the coordinates 
-                temp_img_lst.append(temp_img)
+                temp_img = self.sprite_sheet.subsurface(x * self.sprite_size,y * self.sprite_size,self.sprite_size,self.sprite_size) #subsurface grabs a area around a image given the coordinates 
+                scaled_image = pygame.transform.scale(temp_img,(self.sprite_size * self.image_scale,self.sprite_size * self.image_scale))
+                temp_img_lst.append(scaled_image)
+            # animation_lst.append(temp_img_lst)
+            animation_dict[animation_names[str(y)]] = temp_img_lst
+        # print(animation_dict)
+        return animation_dict
--- constants.py
+++ constants.py
@@ -1,11 +1,15 @@
 import pygame
 
-screen_width,screen_height = 700,500
+screen_width,screen_height = 1000,600
 win = pygame.display.set_mode((screen_width,screen_height)) #inits the game window to set width and height
 run = True #variable for game loop
 fps = pygame.time.Clock() #inits the game clock all computers will run game at the same speed
 level_floor = screen_height - 100 #sets the floor so player doesnt go below it
 warrior_sprite_size = 162 #the individual image size (whole sprite sheet is 1620 pixels wide and there are 10 frames for 162 pixel size for 1 image)
 wizard_sprite_size = 250 #the individual image size (whole sprite sheet is 1620 pixels wide and there are 10 frames for 162 pixel size for 1 image)
-warrior_data = [warrior_sprite_size]
-wizard_data = [wizard_sprite_size]
+warrior_scale = 4
+wizard_scale = 3
+warrior_offset = [72,56]
+wizard_offset = [112,106]
+warrior_data = [warrior_sprite_size,warrior_scale,warrior_offset]
+wizard_data = [wizard_sprite_size,wizard_scale,wizard_offset]
